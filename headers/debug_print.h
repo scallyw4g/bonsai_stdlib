5be9a5d125ae7b0261bd9627bca5a6aacc57c080
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-27 20:25:26 -0600

    "finish" DebugPrint .. at least to the point where it's output is pretty stable

--- headers/debug_print.h
+++ headers/debug_print.h
@@ -64,24 +64,19 @@ DebugPrint(const char *E, u32 Depth = 0)
   DebugChars("%*s%s", Depth, "", E);
 }
 
-/* meta( */
-/*   named_list(external_datatypes) */
-/*   { */
-/*     thing1 thing2 thing3 thing4 */
-/*     test_struct_8 test_struct_16 test_struct_32 test_struct_64 test_struct_128 test_struct_1k */
-/*     head_table ttf_vert ttf_contour simple_glyph font_table ttf offset_subtable ttf_flag */
-/*   } */
-/* ) */
-
-// Note(Jesse): This is, for the purposes of DebugPrint at least, a primitive
-// type and needs to be implemented by hand
+// NOTE(Jesse): For the purposes of DebugPrint, counted_string needs to be implemented by hand
 meta( named_list(project_primitives) { counted_string })
-// TODO(Jesse id: 185, tags: bug, high_priority): these should be printable!
+
+// TODO(Jesse id: 185, tags: bug): These contain function
+// definitions and we currently don't have a way of handling functions when
+// iterating over stuff.  Should be pretty easy to add one of these days
 meta( named_list(buggy_datatypes) { opengl debug_timed_function debug_state })
+
+// NOTE(Jesse): d_unions (for the moment) have a different implementation of DebugPrint
 meta( named_list(d_unions) { ast_node datatype declaration} )
 
-bonsai_function void DebugPrint( ast_node *UnionStruct, u32 Depth = 0);
-bonsai_function void DebugPrint( ast_node UnionStruct, u32 Depth = 0);
+meta(dunion_debug_print_prototype(ast_node))
+#include <poof/generated/debug_print_prototype_ast_node.h>
 
 meta(dunion_debug_print_prototype(datatype))
 #include <poof/generated/debug_print_prototype_datatype.h>
@@ -105,59 +100,47 @@ meta(
 )
 #include <poof/generated/for_all_datatypes_debug_print_prototypes.h>
 
-            /* (Member.is_primitive? */
-            /* { */
-            /*   DebugPrint("(Member.type) (Member.name) = "); */
-            /*   DebugPrint(RuntimeStruct.(Member.name)); */
-            /*   DebugPrint(";"); */
-            /* } */
-            /* { */
-            /*   DebugPrint(RuntimeStruct.(Member.name), Depth+2); */
-            /* }) */
-
 meta(
   for_datatypes(all)
     .exclude(project_primitives buggy_datatypes d_unions)
 
   func (StructDef)
   {
-    bonsai_function void DebugPrint( (StructDef.name) S, u32 Depth)
+    bonsai_function void DebugPrint( (StructDef.name) RuntimeStruct, u32 Depth)
     {
       if (Depth == 0)
       {
         DebugPrint("(StructDef.name) {\n", Depth);
       }
 
-      (
-        StructDef.map_members (Member)
+      (StructDef.map_members (Member)
+      {
+        (Member.is_defined?
         {
-          (Member.is_defined?
+          (Member.name?
           {
-            (Member.name?
+            (Member.is_compound?
             {
-              (Member.is_compound?
-              {
-                DebugPrint("(Member.type) (Member.name) {\n", Depth+2);
-                DebugPrint(S.(Member.name), Depth+4);
-                DebugPrint("}\n", Depth+2);
-              }
-              {
-                DebugPrint("(Member.type) (Member.name) =", Depth+2);
-                DebugPrint(S.(Member.name), 1);
-                DebugPrint(";\n");
-              })
+              DebugPrint("(Member.type) (Member.name) {\n", Depth+2);
+              DebugPrint(RuntimeStruct.(Member.name), Depth+4);
+              DebugPrint("}\n", Depth+2);
             }
             {
-              // NOTE(Jesse): there was an anonymous struct or union here
-              DebugPrint("(Member.type) (Member.name)\n", Depth+2);
+              DebugPrint("(Member.type) (Member.name) =", Depth+2);
+              DebugPrint(RuntimeStruct.(Member.name), 1);
+              DebugPrint(";\n");
             })
           }
           {
-            // NOTE(Jesse): we've got no definition for this type.. print a placeholder
-            DebugPrint("undefined((Member.type) (Member.name))\n", Depth+2);
+            // NOTE(Jesse): an anonymous struct or union
+            DebugPrint("(Member.type) (Member.name)\n", Depth+2);
           })
         }
-      )
+        {
+          // NOTE(Jesse): found no definition for this type.. probably from stdlib
+          DebugPrint("undefined((Member.type) (Member.name))\n", Depth+2);
+        })
+      })
 
       if (Depth == 0)
       {
@@ -165,25 +148,25 @@ meta(
       }
     }
 
-    bonsai_function void DebugPrint( (StructDef.name) *S, u32 Depth)
+    bonsai_function void DebugPrint( (StructDef.name) *RuntimePtr, u32 Depth)
     {
-      if (S) { DebugPrint(*S, Depth); }
+      if (RuntimePtr) { DebugPrint(*RuntimePtr, Depth); }
       else { DebugPrint("ptr(0)\n", Depth); }
     }
   }
 
-  func (EnumDef)
+  func (TEnum)
   {
-    bonsai_function void DebugPrint( (EnumDef.name) EnumValue, u32 Depth)
+    bonsai_function void DebugPrint( (TEnum.name) EnumValue, u32 Depth)
     {
       switch (EnumValue)
       {
         (
-          EnumDef.map_values (ValueDef)
+          TEnum.map_values (TValue)
           {
-            case (ValueDef.name):
+            case (TValue.name):
             {
-              DebugPrint("(ValueDef.name) ", Depth);
+              DebugPrint("(TValue.name)", Depth);
             } break;
           }
         )
