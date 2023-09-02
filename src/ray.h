20c0cf50fefeb456913095838d9141008b11be29
a pirate <jesse.hughes.it@gmail.com>
2023-09-01 18:17:22 -0600

    Return which face of the AABB Intersect() hits

--- src/ray.h
+++ src/ray.h
@@ -24,72 +24,101 @@ struct maybe_ray
   ray Ray;
 };
 
+enum face_index
+{
+  FaceIndex_None,
+
+  FaceIndex_Top,
+  FaceIndex_Bot,
+  FaceIndex_Left,
+  FaceIndex_Right,
+  FaceIndex_Front,
+  FaceIndex_Back,
+};
+
+struct aabb_intersect_result
+{
+  face_index Face;
+  r32 t;
+};
+
 // TODO(Jesse, id: 92, tags: speed, aabb): This could be SIMD optimized ?
 //
 // NOTE(Jesse): Result is tmin value along the ray .. ie closest intersection
 // to origin of the ray.  f32_MAX means no intersection
-link_internal f32
+link_internal aabb_intersect_result
 Intersect(aabb AABB, ray *Ray)
 {
-  v3 min = AABB.Center - AABB.Radius;
-  v3 max = AABB.Center + AABB.Radius;
 
-  r32 tmin = (min.x - Ray->Origin.x) / Ray->Dir.x;
-  r32 tmax = (max.x - Ray->Origin.x) / Ray->Dir.x;
+  v3 AABBMin = AABB.Center - AABB.Radius;
+  v3 AABBMax = AABB.Center + AABB.Radius;
+
+  // Intersection of the the xMin plane
+  r32 tmin = (AABBMin.x - Ray->Origin.x) / Ray->Dir.x;
+  face_index Face = FaceIndex_Left;
+
+  // Intersection of the the xMax plane
+  r32 tmax = (AABBMax.x - Ray->Origin.x) / Ray->Dir.x;
 
   if (tmin > tmax)
   {
     r32 temp = tmin;
     tmin = tmax;
     tmax = temp;
+
+    Face = FaceIndex_Right;
   }
 
-  r32 tymin = (min.y - Ray->Origin.y) / Ray->Dir.y;
-  r32 tymax = (max.y - Ray->Origin.y) / Ray->Dir.y;
+  r32 tymin = (AABBMin.y - Ray->Origin.y) / Ray->Dir.y;
+  r32 tymax = (AABBMax.y - Ray->Origin.y) / Ray->Dir.y;
 
+  face_index yFace = FaceIndex_Front;
   if (tymin > tymax)
   {
     r32 temp = tymin;
     tymin = tymax;
     tymax = temp;
+    yFace = FaceIndex_Back;
   }
 
   if ((tmin > tymax) || (tymin > tmax))
-  return f32_MAX;
+  return { FaceIndex_None, f32_MAX };
 
-  if (tymin > tmin)
-  tmin = tymin;
+  // Intersected the yMin face before the xMin face
+  if (tymin > tmin) { tmin = tymin; Face = yFace; }
 
-  if (tymax < tmax)
-  tmax = tymax;
+  // Intersected the yMax face before the xMax face
+  if (tymax < tmax) tmax = tymax;
 
-  r32 tzmin = (min.z - Ray->Origin.z) / Ray->Dir.z;
-  r32 tzmax = (max.z - Ray->Origin.z) / Ray->Dir.z;
+  r32 tzmin = (AABBMin.z - Ray->Origin.z) / Ray->Dir.z;
+  r32 tzmax = (AABBMax.z - Ray->Origin.z) / Ray->Dir.z;
 
+  face_index zFace = FaceIndex_Top;
   if (tzmin > tzmax)
   {
     r32 temp = tzmin;
     tzmin = tzmax;
     tzmax = temp;
+    zFace = FaceIndex_Bot;
   }
 
   if ((tmin > tzmax) || (tzmin > tmax))
-  return f32_MAX;
+  return { FaceIndex_None, f32_MAX };
 
-  if (tzmin > tmin)
-  tmin = tzmin;
+  // Intersected the zMin face before the yMin face
+  if (tzmin > tmin) { tmin = tzmin; Face = zFace; }
 
-  if (tzmax < tmax)
-  tmax = tzmax;
+  // Intersected the zMax face before the yMax face
+  if (tzmax < tmax) tmax = tzmax;
 
-  return tmin;
+  return {Face, tmin};
 }
 
-// TODO(Jesse): Remove this?
-link_internal f32
-Intersect(aabb AABB, ray Ray)
-{
-  f32 Result = Intersect(AABB, &Ray);
-  return Result;
-}
+/* // TODO(Jesse): Remove this? */
+/* link_internal f32 */
+/* Intersect(aabb AABB, ray Ray) */
+/* { */
+/*   f32 Result = Intersect(AABB, &Ray); */
+/*   return Result; */
+/* } */
 
