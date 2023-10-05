696379caefc3d92b4e12416b63de1e44ba9b5387
a pirate <jesse.hughes.it@gmail.com>
2023-10-05 12:16:41 -0600

    Add missing braces in SafeCast

--- src/iterators.h
+++ src/iterators.h
@@ -46,7 +46,7 @@
 #define TryCast(T, Ptr) ((Ptr)->Type == type_##T ? (&(Ptr)->T) : 0)
 #define SafeCast(T, Ptr) (T*)(&(Ptr)->T); Assert((Ptr)->Type == type_##T)
 
-#define SafeAccess(T, Ptr) SafeCast(T, Ptr)
+#define SafeAccess(T, Ptr) SafeCast(T, (Ptr))
 #define SafeAccessPtr(T, Ptr) ((Ptr)->T); Assert((Ptr)->Type == type_##T)
 
 #define ITERATE_OVER_AS(prefix, value_ptr)        \
