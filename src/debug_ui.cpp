1f45311e13632098f95e747be4e6c1daa69c842d
a pirate <jesse.hughes.it@gmail.com>
2023-10-14 08:22:32 -0600

    Add debug_ui.(h|cpp)

---
+++ src/debug_ui.cpp
@@ -0,0 +1,35 @@
+r32
+GetXOffsetForHorizontalBar(u64 StartCycleOffset, u64 FrameTotalCycles, r32 TotalGraphWidth)
+{
+  r32 XOffset = ((r32)StartCycleOffset/(r32)FrameTotalCycles)*TotalGraphWidth;
+  return XOffset;
+}
+
+
+link_internal void
+PushCycleBar(debug_ui_render_group* Group, cycle_range* Range, cycle_range* Frame, r32 TotalGraphWidth, r32 BarHeight, r32 yOffset, ui_style *Style, v4 Padding = V4(0), cs Name = CSz(""))
+{
+  Assert(Frame->StartCycle <= Range->StartCycle);
+
+  r32 FramePerc = (r32)Range->TotalCycles / (r32)Frame->TotalCycles;
+
+  r32 BarWidth = FramePerc*TotalGraphWidth;
+
+  if (BarWidth > 0.15f)
+  {
+    v2 BarDim = V2(BarWidth, BarHeight);
+
+    u64 StartCycleOffset = Range->StartCycle - Frame->StartCycle;
+    r32 xOffset = GetXOffsetForHorizontalBar(StartCycleOffset, Frame->TotalCycles, TotalGraphWidth);
+
+    v2 Offset = V2(xOffset, yOffset);
+
+    PushUntexturedQuad(Group, Offset, BarDim, zDepth_Text, Style, Padding, QuadRenderParam_AdvanceClip);
+    ui_style NameStyle = DefaultStyle;
+    NameStyle.Font = Global_SmallFont;
+    if (Name.Count) { Text(Group, Name, &NameStyle, TextRenderParam_NoAdvanceLayout, Offset, RectMinDim({}, BarDim)); }
+  }
+
+  return;
+}
+
---
+++ src/debug_ui.h
@@ -0,0 +1,25 @@
+
+
+link_internal u8
+GetByte(u32 ByteIndex, u64 Source)
+{
+  u32 ShiftWidth = ByteIndex * 8;
+  Assert(ShiftWidth < (sizeof(Source)*8));
+
+  u64 Mask = (u64)(0xff << ShiftWidth);
+  u8 Result = (u8)((Source & Mask) >> ShiftWidth);
+  return Result;
+}
+
+link_internal v3
+ColorFromHash(u64 HashValue)
+{
+  u8 R = GetByte(0, HashValue);
+  u8 G = GetByte(1, HashValue);
+  u8 B = GetByte(2, HashValue);
+
+  v3 Color = V3( R / 255.0f, G / 255.0f, B / 255.0f );
+  return Color;
+}
+
+
