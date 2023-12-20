49a75336dbf99916472413ecb771c61a5b30c85c
a pirate <jesse.hughes.it@gmail.com>
2023-12-20 09:40:21 -0800

    Remove a dead comment

--- src/colors.cpp
+++ src/colors.cpp
@@ -1,8 +1,6 @@
 inline v3
 GetColorData(v3_cursor *Palette, u32 ColorIndex)
 {
-  /* Assert(ColorIndex < u32(Global_ColorPaletteAt)); */
-  /* v3_cursor *Palette = GetEngineResources()->World.ColorPalette; */
   v3 Result = Palette->Start[ColorIndex] / 255.f;
   return Result;
 }
