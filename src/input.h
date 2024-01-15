9660cccaa493275208cfb124473b6896f97063d1
a pirate <jesse.hughes.it@gmail.com>
2024-01-15 11:06:31 -0800

    Remove a pile of useless hotkeys

--- src/input.h
+++ src/input.h
@@ -76,27 +76,10 @@ struct hotkeys
   b32 Debug_TriangulateIncrement;
   b32 Debug_TriangulateDecrement;
 
-  b32 Debug_PickChunks_All;
-  b32 Debug_PickChunks_Terrain;
-  b32 Debug_PickChunks_Voxel;
-
-  b32 Debug_Action_ComputeStandingSpot;
-
-  b32 Debug_RedrawEveryPush;
-  b32 Debug_ToggleLoopedGamePlayback;
-  b32 Debug_ToggleTriggeredRuntimeBreak;
-
-  b32 Debug_Pause;
-
   b32 Left;
   b32 Right;
   b32 Forward;
   b32 Backward;
-
-  b32 Player_Fire;
-  b32 Player_Proton;
-  b32 Player_Jump;
-  b32 Player_Spawn;
 };
 
 inline v3
@@ -129,25 +112,11 @@ BindHotkeysToInput(hotkeys *Hotkeys, input *Input)
 {
 
 #if BONSAI_INTERNAL
-  Hotkeys->Debug_Pause                    = Input->F12.Pressed;
-  Hotkeys->Debug_ToggleLoopedGamePlayback = Input->F11.Clicked;
-
-
   if (Input->F1.Clicked) { Hotkeys->Debug_ToggleMenu         = True; }
   if (Input->F2.Clicked) { Hotkeys->Debug_ToggleProfiling    = True; }
 
-  // NOTE(Jesse): Intentionally 'Pressed'
-  if (Input->F5.Pressed) { Hotkeys->Debug_PickChunks_All     = True; }
-  if (Input->F6.Pressed) { Hotkeys->Debug_PickChunks_Terrain = True; }
-  if (Input->F7.Pressed) { Hotkeys->Debug_PickChunks_Voxel   = True; }
-
-  if (Input->F9.Pressed) { Hotkeys->Debug_Action_ComputeStandingSpot = True; }
-
   /* Hotkeys->Debug_TriangulateDecrement = Input->F5.Clicked; */
   /* Hotkeys->Debug_TriangulateIncrement = Input->F6.Clicked; */
-
-  /* Hotkeys->Debug_MousePick = Input->F12.Pressed; */
-  /* Hotkeys->Debug_RedrawEveryPush = Input->F2.Clicked; */
 #endif
 
   Hotkeys->Left = Input->A.Pressed;
@@ -157,12 +126,6 @@ BindHotkeysToInput(hotkeys *Hotkeys, input *Input)
 
   /* Hotkeys->Player_Fire = Input->Space.Clicked; */
   /* Hotkeys->Player_Proton = Input->Shift.Clicked; */
-
-  Hotkeys->Player_Jump = Input->Space.Clicked;
-
-  Hotkeys->Player_Spawn = Input->Enter.Clicked;
-
-  return;
 }
 
 link_internal void
