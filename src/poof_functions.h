5b0bb553dbc8ffd425248dcad6e7d6f529b9ff85
a pirate <jesse.hughes.it@gmail.com>
2024-08-27 09:35:32 -0700

    Fix crash if AreEqual was passed null pointers

--- src/poof_functions.h
+++ src/poof_functions.h
@@ -611,23 +611,30 @@ poof(
     link_internal b32
     AreEqual((Type.name) *Thing1, (Type.name) *Thing2)
     {
-      Type.contains_type(cs)?
+      if (Thing1 && Thing2)
       {
-        b32 Result = True;
-        Type.map(member)
+        Type.contains_type(cs)?
         {
-          member.is_function?
+          b32 Result = True;
+          Type.map(member)
           {
-          }
-          {
-            Result &= AreEqual(Thing1->member.name, Thing2->member.name);
+            member.is_function?
+            {
+            }
+            {
+              Result &= AreEqual(Thing1->member.name, Thing2->member.name);
+            }
           }
         }
+        {
+          b32 Result = MemoryIsEqual((u8*)Thing1, (u8*)Thing2, sizeof( (Type.name) ) );
+        }
+        return Result;
       }
+      else
       {
-        b32 Result = MemoryIsEqual((u8*)Thing1, (u8*)Thing2, sizeof( (Type.name) ) );
+        return (Thing1 == Thing2);
       }
-      return Result;
     }
 
     link_internal b32
