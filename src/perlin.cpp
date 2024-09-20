565b5009f80704ace2bf81de8011bfae3443e724
a pirate <jesse.hughes.it@gmail.com>
2024-09-20 10:06:06 -0700

    Try out Chris Wellons' integer hash

--- src/perlin.cpp
+++ src/perlin.cpp
@@ -1,37 +1,32 @@
 
-#if 0
-#define SETUP_HASH_DATA_FOR_SIMD_LANE(lane_number) \
-    u32 Xi_##lane_number = _Xi[Index+lane_number]; \
-    f32  x_##lane_number = _xF[Index+lane_number]; \
-    u32  A_##lane_number = Global_PerlinIV[ Xi_##lane_number  ] + Yi;      \
-    u32 AA_##lane_number = Global_PerlinIV[  A_##lane_number  ] + Zi;      \
-    u32 AB_##lane_number = Global_PerlinIV[  A_##lane_number+1] + Zi;      \
-    u32  B_##lane_number = Global_PerlinIV[ Xi_##lane_number+1] + Yi;      \
-    u32 BA_##lane_number = Global_PerlinIV[  B_##lane_number  ] + Zi;      \
-    u32 BB_##lane_number = Global_PerlinIV[  B_##lane_number+1] + Zi;      \
-    \
-    u32 H0_##lane_number = Global_PerlinIV[ AA_##lane_number  ];            \
-    u32 H1_##lane_number = Global_PerlinIV[ BA_##lane_number  ];            \
-    u32 H2_##lane_number = Global_PerlinIV[ AB_##lane_number  ];            \
-    u32 H3_##lane_number = Global_PerlinIV[ BB_##lane_number  ];            \
-    u32 H4_##lane_number = Global_PerlinIV[ AA_##lane_number+1];            \
-    u32 H5_##lane_number = Global_PerlinIV[ BA_##lane_number+1];            \
-    u32 H6_##lane_number = Global_PerlinIV[ AB_##lane_number+1];            \
-    u32 H7_##lane_number = Global_PerlinIV[ BB_##lane_number+1]
-#else
 u32_8x
 HashPrimes(u32_8x Seed, u32_8x x, u32_8x y, u32_8x z)
 {
   u32_8x hash = Seed;
 
+#if 0
+  hash = hash ^ x ^ y ^ z;
+#else
   hash = hash ^ x;
   hash = hash ^ y;
   hash = hash ^ z;
-
   hash = hash * U32_8X( 0x27d4eb2d );
+#endif
   return (hash >> 15) ^ hash;
 }
-#endif
+
+// https://nullprogram.com/blog/2018/07/31/
+//
+u32_8x
+ChrisWellonsIntegerHash_lowbias32(u32_8x x)
+{
+  x = x ^ (x >> 16);
+  x = x * U32_8X(0x7feb352d);
+  x = x ^ (x >> 15);
+  x = x * U32_8X(0x846ca68b);
+  x = x ^ (x >> 16);
+  return x;
+}
 
 #if 0
 link_internal void
@@ -198,6 +193,80 @@ PerlinNoise_8x_avx2(f32 *xIn, f32 yIn, f32 zIn, f32 *Result)
     f32_8x G5 = Grad8x(HashPrimes(Seed, x1, y0, z1), xf1, yf0, zf1);
     f32_8x G6 = Grad8x(HashPrimes(Seed, x0, y1, z1), xf0, yf1, zf1);
     f32_8x G7 = Grad8x(HashPrimes(Seed, x1, y1, z1), xf1, yf1, zf1);
+
+    auto L0  = Lerp8x(xs, G0, G1);
+    auto L1  = Lerp8x(xs, G2, G3);
+    auto L2  = Lerp8x(xs, G4, G5);
+    auto L3  = Lerp8x(xs, G6, G7);
+
+    auto L4  = Lerp8x(ys, L0, L1);
+    auto L5  = Lerp8x(ys, L2, L3);
+
+    auto Res = Lerp8x(zs, L4, L5);
+    /* Res = Res * F32_8X( 0.964921414852142333984375f ); */
+    Res = (Res + F32_8X(1.f)) / F32_8X(2.f);
+
+    Result[Index+0] = Res.E[0];
+    Result[Index+1] = Res.E[1];
+    Result[Index+2] = Res.E[2];
+    Result[Index+3] = Res.E[3];
+    Result[Index+4] = Res.E[4];
+    Result[Index+5] = Res.E[5];
+    Result[Index+6] = Res.E[6];
+    Result[Index+7] = Res.E[7];
+  }
+}
+
+link_internal void
+PerlinNoise_16x_avx2(f32 *xIn, f32 yIn, f32 zIn, f32 *Result)
+{
+  auto PrimeX = U32_8X(501125321);
+  auto PrimeY = U32_8X(1136930381);
+  auto PrimeZ = U32_8X(1720413743);
+  auto PrimeW = U32_8X(1066037191);
+
+  auto  Seed = U32_8X(1066037191);
+
+  /* u32 Index = 0; */
+  for (u32 Index = 0; Index < 16; Index += 8)
+  {
+    auto x = F32_8X( xIn[Index+0], xIn[Index+1], xIn[Index+2], xIn[Index+3], xIn[Index+4], xIn[Index+5], xIn[Index+6], xIn[Index+7] );
+    auto y = F32_8X(yIn);
+    auto z = F32_8X(zIn);
+
+    auto xs = Floor(x);
+    auto ys = Floor(y);
+    auto zs = Floor(z);
+
+    auto x0 = U32_8X(xs) * PrimeX;
+    auto y0 = U32_8X(ys) * PrimeY;
+    auto z0 = U32_8X(zs) * PrimeZ;
+
+    auto x1 = x0 + PrimeX;
+    auto y1 = y0 + PrimeY;
+    auto z1 = z0 + PrimeZ;
+
+    auto xf0 = xs = x - xs;
+    auto yf0 = ys = y - ys;
+    auto zf0 = zs = z - zs;
+
+    auto xf1 = xf0 - F32_8X(1.f);
+    auto yf1 = yf0 - F32_8X(1.f);
+    auto zf1 = zf0 - F32_8X(1.f);
+
+    xs = Fade8x(xs);
+    ys = Fade8x(ys);
+    zs = Fade8x(zs);
+
+    f32_8x G0 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y0+z0), xf0, yf0, zf0);
+    f32_8x G1 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y0+z0), xf1, yf0, zf0);
+    f32_8x G2 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y1+z0), xf0, yf1, zf0);
+    f32_8x G3 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y1+z0), xf1, yf1, zf0);
+
+    f32_8x G4 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y0+z1), xf0, yf0, zf1);
+    f32_8x G5 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y0+z1), xf1, yf0, zf1);
+    f32_8x G6 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y1+z1), xf0, yf1, zf1);
+    f32_8x G7 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y1+z1), xf1, yf1, zf1);
 
     auto L0  = Lerp8x(xs, G0, G1);
     auto L1  = Lerp8x(xs, G2, G3);
