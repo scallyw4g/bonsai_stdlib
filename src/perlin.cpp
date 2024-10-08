457781891d259681ecff3df913a50b788fb4dbaf
a pirate <jesse.hughes.it@gmail.com>
2024-10-07 17:33:00 -0700

    Minor cleanup

--- src/perlin.cpp
+++ src/perlin.cpp
@@ -373,55 +373,29 @@ PerlinNoise_16x_avx2_y(perlin_params *perlinX, perlin_params *perlinY, perlin_pa
 link_internal void
 PerlinNoise_8x_avx2(perlin_params *perlinX, perlin_params *perlinY, perlin_params *perlinZ, f32 *Dest, f32 Amplitude)
 {
-  auto PrimeX = U32_8X(501125321);
+  u32_8x Seed = U32_8X(1066037191);
 
-/* #pragma unroll(2) */
-    u32 Index = 0;
-/*   for (u32 Index = 0; Index < 16; Index += 8) */
-  {
-    // Fire off the load for the current values so it has time to cook
-    f32_8x Current = {{ _mm256_load_ps(Dest+Index) }};
+  f32_8x G0 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P0, perlinZ->P0), perlinX->Fract0, perlinY->Fract0, perlinZ->Fract0);
+  f32_8x G1 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P0, perlinZ->P0), perlinX->Fract1, perlinY->Fract0, perlinZ->Fract0);
+  f32_8x G2 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P1, perlinZ->P0), perlinX->Fract0, perlinY->Fract1, perlinZ->Fract0);
+  f32_8x G3 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P1, perlinZ->P0), perlinX->Fract1, perlinY->Fract1, perlinZ->Fract0);
 
-#if 0
-    f32_8x G0 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y0+z0), xf0, yf0, zf0);
-    f32_8x G1 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y0+z0), xf1, yf0, zf0);
-    f32_8x G2 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y1+z0), xf0, yf1, zf0);
-    f32_8x G3 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y1+z0), xf1, yf1, zf0);
+  f32_8x G4 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P0, perlinZ->P1), perlinX->Fract0, perlinY->Fract0, perlinZ->Fract1);
+  f32_8x G5 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P0, perlinZ->P1), perlinX->Fract1, perlinY->Fract0, perlinZ->Fract1);
+  f32_8x G6 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P1, perlinZ->P1), perlinX->Fract0, perlinY->Fract1, perlinZ->Fract1);
+  f32_8x G7 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P1, perlinZ->P1), perlinX->Fract1, perlinY->Fract1, perlinZ->Fract1);
 
-    f32_8x G4 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y0+z1), xf0, yf0, zf1);
-    f32_8x G5 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y0+z1), xf1, yf0, zf1);
-    f32_8x G6 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x0+y1+z1), xf0, yf1, zf1);
-    f32_8x G7 = Grad8x(ChrisWellonsIntegerHash_lowbias32(x1+y1+z1), xf1, yf1, zf1);
+  auto L0  = Lerp8x(perlinX->Fade, G0, G1);
+  auto L1  = Lerp8x(perlinX->Fade, G2, G3);
+  auto L2  = Lerp8x(perlinX->Fade, G4, G5);
+  auto L3  = Lerp8x(perlinX->Fade, G6, G7);
 
-#else
-    u32_8x Seed = U32_8X(1066037191);
+  auto L4  = Lerp8x(perlinY->Fade, L0, L1);
+  auto L5  = Lerp8x(perlinY->Fade, L2, L3);
 
-    f32_8x G0 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P0, perlinZ->P0), perlinX->Fract0, perlinY->Fract0, perlinZ->Fract0);
-    f32_8x G1 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P0, perlinZ->P0), perlinX->Fract1, perlinY->Fract0, perlinZ->Fract0);
-    f32_8x G2 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P1, perlinZ->P0), perlinX->Fract0, perlinY->Fract1, perlinZ->Fract0);
-    f32_8x G3 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P1, perlinZ->P0), perlinX->Fract1, perlinY->Fract1, perlinZ->Fract0);
+  auto Res = Lerp8x(perlinZ->Fade, L4, L5) * F32_8X(Amplitude);
 
-    f32_8x G4 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P0, perlinZ->P1), perlinX->Fract0, perlinY->Fract0, perlinZ->Fract1);
-    f32_8x G5 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P0, perlinZ->P1), perlinX->Fract1, perlinY->Fract0, perlinZ->Fract1);
-    f32_8x G6 = Grad8x_fast(HashPrimes(Seed, perlinX->P0, perlinY->P1, perlinZ->P1), perlinX->Fract0, perlinY->Fract1, perlinZ->Fract1);
-    f32_8x G7 = Grad8x_fast(HashPrimes(Seed, perlinX->P1, perlinY->P1, perlinZ->P1), perlinX->Fract1, perlinY->Fract1, perlinZ->Fract1);
-#endif
-
-    auto L0  = Lerp8x(perlinX->Fade, G0, G1);
-    auto L1  = Lerp8x(perlinX->Fade, G2, G3);
-    auto L2  = Lerp8x(perlinX->Fade, G4, G5);
-    auto L3  = Lerp8x(perlinX->Fade, G6, G7);
-
-    auto L4  = Lerp8x(perlinY->Fade, L0, L1);
-    auto L5  = Lerp8x(perlinY->Fade, L2, L3);
-
-    auto Res = Lerp8x(perlinZ->Fade, L4, L5);
-    /* Res = Res * F32_8X( 0.964921414852142333984375f ); */
-    Res = ((Res + F32_8X(1.f)) / F32_8X(2.f)) * F32_8X(Amplitude);
-
-    f32_8x Total = Res + Current;
-    _mm256_store_ps(Dest+Index, Total.Sse);
-
-    ++perlinX;
-  }
+  f32_8x Current = {{ _mm256_load_ps(Dest) }};
+  f32_8x Total = Res + Current;
+  _mm256_store_ps(Dest, Total.Sse);
 }
