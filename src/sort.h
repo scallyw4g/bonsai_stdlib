68b3cba0ef801c6458ec8cd121d113ea2a0ca09b
a pirate <jesse.hughes.it@gmail.com>
2024-11-07 17:08:25 -0700

    Add descending sort

--- src/sort.h
+++ src/sort.h
@@ -4,18 +4,26 @@ struct sort_key_string
   u64 Index;
   cs Value;
 
-  b32 operator<(sort_key_string &Cmp)
-  {
-    return (CompareStrings(&this->Value, &Cmp.Value) < 0);
-  }
+  b32 operator<(sort_key_string &Cmp) { return (CompareStrings(&this->Value, &Cmp.Value) < 0); }
+  b32 operator>(sort_key_string &Cmp) { return (CompareStrings(&this->Value, &Cmp.Value) > 0); }
 };
 
-struct sort_key_f
+struct sort_key_f64
 {
   u64 Index;
   r64 Value;
 
-  b32 operator<(sort_key_f &Cmp) { return this->Value < Cmp.Value; }
+  b32 operator<(sort_key_f64 &Cmp) { return this->Value < Cmp.Value; }
+  b32 operator>(sort_key_f64 &Cmp) { return this->Value > Cmp.Value; }
+};
+
+struct sort_key_f32
+{
+  u64 Index;
+  f32 Value;
+
+  b32 operator<(sort_key_f32 &Cmp) { return this->Value < Cmp.Value; }
+  b32 operator>(sort_key_f32 &Cmp) { return this->Value > Cmp.Value; }
 };
 
 struct sort_key
@@ -24,6 +32,7 @@ struct sort_key
   u64 Value;
 
   b32 operator<(sort_key &Cmp) { return this->Value < Cmp.Value; }
+  b32 operator>(sort_key &Cmp) { return this->Value > Cmp.Value; }
 };
 
 poof(
@@ -61,11 +70,42 @@ poof(
 
         return;
       }
+
+      link_internal void
+      BubbleSort_descending((sort_key_t.name) *Keys, u32 Count)
+      {
+        for (u32 Ignored = 0;
+            Ignored < Count;
+            ++Ignored)
+        {
+          b32 Sorted = True;
+
+          for (u32 Inner = 0;
+              Inner < (Count-1);
+              ++Inner)
+          {
+            auto *KeyA = Keys+Inner;
+            auto *KeyB = Keys+Inner+1;
+
+            if (*KeyA > *KeyB)
+            {
+              auto Temp = *KeyA;
+              *KeyA = *KeyB;
+              *KeyB = Temp;
+              Sorted = False;
+            }
+          }
+
+          if (Sorted) break;
+        }
+
+        return;
+      }
     }
   }
 )
 
-poof(bubble_sort({sort_key sort_key_f sort_key_string}))
+poof(bubble_sort({sort_key sort_key_f64 sort_key_f32 sort_key_string}))
 #include <generated/bubble_sort_686708022.h>
 
 link_internal void
