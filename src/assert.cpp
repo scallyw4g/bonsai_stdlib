9eb81d3714b87b6646af4708c11e944da7c171c7
a pirate <jesse.hughes.it@gmail.com>
2025-11-05 18:55:57 -0700

    Add BONSAI_SOFT_ASSERT ifdef

--- src/assert.cpp
+++ src/assert.cpp
@@ -3,6 +3,8 @@ void assert_fail(cs FmtString, cs Condition, const char *Function)
   LogDirect(CSz("%S ! FAILED%S  - "), TerminalColors.Red, TerminalColors.White);
   LogDirect(FmtString, Condition, Function);
 
+#ifndef BONSAI_SOFT_ASSERT
   RuntimeBreak();
+#endif
 }
 
